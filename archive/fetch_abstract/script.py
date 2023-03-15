40ab9f82b7e4f71d81515fd7876b73126bb8d47c
Pierre-Olivier Côté <pierre-olivier.cote@polymtl.ca>
2023-03-15 09:35:35 -0400

    added fetch abstract script that has never been used

--- archive/fetch_abstract/script.py
+++ archive/fetch_abstract/script.py
@@ -28,7 +28,7 @@ def save_to_csv(df):
     crnt_path = Path(__file__).parent.resolve()
     out_path = crnt_path/"out"
     os.makedirs(out_path, exist_ok=True)
-    df.to_csv(out_path/"res.csv", index=False)
+    df.to_csv(out_path/"res.csv", index=False, errors="replace")
 
 
 def add_noisy_abstract(papers):
@@ -50,7 +50,8 @@ def add_noisy_abstract(papers):
 if __name__ == '__main__':
     crnt_dir = Path(__file__).parent.resolve()
     papers = pd.read_csv(crnt_dir/'lib_gs.csv')
-    # papers = papers.iloc[0:500]
+    papers = papers.iloc[0:100]
     noisy_abstracts = add_noisy_abstract(papers)
     papers['noisy_abstract'] = noisy_abstracts
+    papers.to_pickle(crnt_dir/'out'/'res.pkl')
     save_to_csv(papers)
